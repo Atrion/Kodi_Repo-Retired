13d3a8468d2d9c57e8e6c56302a27c329b3d8d5f
Atrion <Atrion@gmail.com>
2017-12-17 18:11:22 -0400

    Updated Neptune and Repos

--- plugin.video.neptune/resources/lib/indexers/navigator.py
+++ plugin.video.neptune/resources/lib/indexers/navigator.py
@@ -295,6 +295,14 @@ class navigator:
 		self.addDirectoryItem('Virtual Reality', 'movies&url=vr', 'vr.jpg', 'playlist.jpg')
 		self.addDirectoryItem('Wilhelm Scream', 'movies&url=wilhelm', 'wilhelm.jpg', 'playlist.jpg')
 		self.addDirectoryItem('Zombies', 'movies&url=zombie', 'zombie.jpg', 'playlist.jpg')
+		self.addDirectoryItem('New Years', 'movies&url=newyear', 'season.jpg', 'season.jpg')
+		self.addDirectoryItem('Easter', 'movies&url=easter', 'season.jpg', 'season.jpg')
+		self.addDirectoryItem('Halloween', 'movies&url=halloween', 'season.jpg', 'season.jpg')
+		self.addDirectoryItem('Thanksgiving', 'movies&url=thanx', 'season.jpg', 'season.jpg')
+		self.addDirectoryItem('Christmas', 'movies&url=xmass', 'season.jpg', 'season.jpg')
+		self.addDirectoryItem('DC', 'movies&url=dc', 'playlist.jpg', 'playlist.jpg')
+		self.addDirectoryItem('Disney and Pixar', 'movies&url=disney', 'playlist.jpg', 'playlist.jpg')
+		self.addDirectoryItem('Marvel Universe', 'movies&url=marvel', 'playlist.jpg', 'playlist.jpg')
 
 		self.endDirectory()		
 
---
+++ script.module.urllib.3/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""
+urllib3 - Thread-safe connection pooling and re-using.
+"""
+
+from resources.lib import __init__
